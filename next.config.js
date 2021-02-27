35b49243d1432a253b6c82758f0963e5d2491560
Krishan Patel <krishanpatel09@gmail.com>
2021-02-26 20:41:47 -0500

    Change build directory name

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/krshnpatel/krshnpatel.github.io/gh-pages/' : '',
+  distDir: 'build'
 }
